cdeaaec3824bc3780ac5fc86fbbc87d9b7d75b47
Daniel Karolak <danielkarolak67@gmail.com>
2024-10-23 17:35:44 +0200

    Added Home Page

---
+++ settings/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Project</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="navbar">
+        <a href="#" class="logo">TechCare</a>
+        <div class="links">
+            <a href="#">Home</a>
+            <a href="about">About</a>
+            <a href="services">Services</a>
+            <a href="contact">Contact</a>
+            <a href="settings">Settings</a>
+            <a href="login">
+                <button class="button">Login</button>
+            </a>
+        </div>
+        <button class="mobile-button">
+            <span class="menu-line"></span>
+            <span class="menu-line"></span>
+            <span class="menu-line"></span>
+        </button>
+        <div class="mobile-links">
+            <a href="#" class="mobile-link">Home</a>
+            <a href="about" class="mobile-link">About</a>
+            <a href="services" class="mobile-link">Services</a>
+            <a href="contact" class="mobile-link">Contact</a>
+            <a href="settings" class="mobile-link">Settings</a>
+            <a href="login">
+                <button class="button">Login</button>
+            </a>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div>Daniel Karolak © 2024</div>
+    </div>
+</div>
+</body>
+</html>
